66e2359180f9031d0ab57877214e796634486078
Hadasah <06141308@gm.scu.edu.tw>
2021-07-11 21:16:36 +0800

    first version

---
+++ hadasah1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hadasah1
+//
+//  Created by yangsc on 2021/7/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
